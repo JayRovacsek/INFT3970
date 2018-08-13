a702908d3ec1186795b237c3c8cb82120d1a845d
Jay Rovacsek <c3146220@uon.edu.au>
2018-08-13 15:33:28 +1000

    Some clean-up and further implementation

--- INFT3970-Project/INFT3970-Project/Controllers/HomeController.cs
+++ INFT3970-Project/INFT3970-Project/Controllers/HomeController.cs
@@ -4,7 +4,7 @@ using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
-using INFT3970_Project.Models;
+using INFT3970Project.Models;
 
 namespace INFT3970Project.Controllers
 {
