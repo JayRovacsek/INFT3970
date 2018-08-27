a923eada7ddd81b4b975e8e65622b1ea3cdecab9
Jay Rovacsek <jayrovacsek@gmail.com>
2018-08-27 21:33:51 +1000

    Implement PING API

---
+++ INFT3970-Project/INFT3970-Project/Controllers/AvailabilityController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace INFT3970Project.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/Availability")]
+    public class AvailabilityController : Controller
+    {
+        [HttpGet]
+        public HttpResponseMessage Get()
+        {
+            return new HttpResponseMessage(System.Net.HttpStatusCode.OK);
+        }
+    }
+}
