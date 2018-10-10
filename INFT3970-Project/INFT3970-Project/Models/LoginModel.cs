dbd9ab6bdcb8087aba540906f087b7c4dcf80599
Jay Rovacsek <jayrovacsek@gmail.com>
2018-10-10 20:42:49 +1100

    Start moving towards auth

--- INFT3970-Project/INFT3970-Project/Models/LoginModel.cs
+++ INFT3970-Project/INFT3970-Project/Models/LoginModel.cs
@@ -9,6 +9,6 @@ namespace INFT3970Project.Models
         public string Username { get; set; }
         [Required]
         public string Password { get; set; }
-        public bool SuccessfulLogin { get; set; }
+        public bool SuccessfulLogin { get; set; } = false;
     }
 }
