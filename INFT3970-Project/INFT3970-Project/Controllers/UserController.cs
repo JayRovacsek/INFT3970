8805c676ec8b3d342a28ed4743cd46b6d3e73afd
Jay Rovacsek <jayrovacsek@gmail.com>
2018-10-16 23:15:22 +1100

    Web Application work, numerous changes

---
+++ INFT3970-Project/INFT3970-Project/Controllers/UserController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using INFT3970Project.Helpers;
+using INFT3970Project.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+
+namespace INFT3970Project.Controllers
+{
+    [Authorize]
+    public class UserController : Controller
+    {
+        private readonly IConfiguration configuration;
+        private DatabaseHelper _databaseHelper;
+
+        public UserController(IConfiguration configuration, DatabaseHelper databaseHelper)
+        {
+            this.configuration = configuration;
+            _databaseHelper = databaseHelper;
+        }
+
+        public IActionResult Manage()
+        {
+            var userSensorPasswordViewModel = _databaseHelper.GetUserSensorAndPasswordViewModel(1);
+            //throw new ApplicationException("Need to fix this to get userId");
+            return View(userSensorPasswordViewModel);
+        }
+
+
+        [HttpPost]
+        public IActionResult UpdatePassword(LoginModel model)
+        {
+            // UPDATE PASSWORD HERE
+
+            var success = true;
+
+            if (success)
+            {
+                return RedirectToAction("Index", "User", "Successfully updated password");
+            }
+            return RedirectToAction("Index", "User", "Unable to update password");
+        }
+    }
+}
---
+++ INFT3970-Project/INFT3970-Project/Models/Database Entities/RoomModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace INFT3970Project.Models
+{
+    public class RoomModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+    }
+}
