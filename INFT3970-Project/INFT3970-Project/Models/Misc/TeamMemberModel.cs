dce2d199e12233e84e53e40c134117bbc097f5c8
Jay Rovacsek <jayrovacsek@gmail.com>
2018-10-17 22:50:09 +1100

    Number of changes, extending functionality

---
+++ INFT3970-Project/INFT3970-Project/Models/Misc/TeamMemberModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace INFT3970Project.Models.Misc
+{
+    public class TeamMemberModel
+    {
+        public string Email { get; set; }
+        public string Github { get; set; }
+        public string LinkedIn { get; set; }
+
+        public TeamMemberModel(string email, string github, string linkedIn)
+        {
+            Email = email;
+            Github = github;
+            LinkedIn = linkedIn;
+        }
+    }
+}
