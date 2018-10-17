0984cc3b738a5fbb8ebe9c5cc6406dff79b4ff51
Jay Rovacsek <jayrovacsek@gmail.com>
2018-10-17 18:13:50 +1100

    Updated layout templates to include github link, imported bs4

---
+++ INFT3970-Project/INFT3970-Project/Models/Database Entities/ViewModels/UserAndPasswordModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace INFT3970Project.Models
+{
+    public class UserAndPasswordModel
+    {
+        public UserModel User { get; set; }
+        public UserPasswordModel UserPassword { get; set; }
+    }
+}
