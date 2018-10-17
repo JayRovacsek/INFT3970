4b0efbd21be4973cbe5678bc4840da0bc447f6ec
Jay Rovacsek <jayrovacsek@gmail.com>
2018-10-17 21:33:57 +1100

    Fixing issues with merge

--- INFT3970-Project/INFT3970-Project/Models/Database Entities/UserPasswordModel.cs
+++ INFT3970-Project/INFT3970-Project/Models/Database Entities/UserPasswordModel.cs
@@ -8,6 +8,7 @@ namespace INFT3970Project.Models
     public class UserPasswordModel
     {
         public int UserID { get; set; }
+        public string Password { get; set; }
         public string HashedPassword { get; set; }
         public int Salt { get; set; }
     }
