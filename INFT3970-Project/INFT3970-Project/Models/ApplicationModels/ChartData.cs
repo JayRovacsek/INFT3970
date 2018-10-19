cee8a23e3a36cbbea579b0ebb6ba1b3864368ec0
Jay Rovacsek <jayrovacsek@gmail.com>
2018-10-19 19:17:17 +1100

    Getting closer.

---
+++ INFT3970-Project/INFT3970-Project/Models/ApplicationModels/ChartData.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace INFT3970Project.Models.ApplicationModels
+{
+    public class ChartData
+    {
+        public double x { get; set; }
+        public decimal y { get; set; }
+    }
+}
