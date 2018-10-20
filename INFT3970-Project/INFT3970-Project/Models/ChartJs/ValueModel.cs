354bb48595dffe21dd01ece5edb566397a05b062
Jay Rovacsek <jayrovacsek@gmail.com>
2018-10-20 12:01:12 +1100

    Fixed issues with previous iteration, created json serialised classes to create charts

---
+++ INFT3970-Project/INFT3970-Project/Models/ChartJs/ValueModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace INFT3970Project.Models
+{
+    public class ValueModel
+    {
+        public double x { get; set; }
+        public decimal y { get; set; }
+    }
+}
