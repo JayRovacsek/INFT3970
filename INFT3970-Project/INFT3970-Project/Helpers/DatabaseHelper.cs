c64ef11c12cb260ef143c564cdcce5d2790ed43b
DeanMorty <c3252227@uon.edu.au>
2018-10-22 20:07:45 +1100

    AddRoom and AddSensor Work

--- INFT3970-Project/INFT3970-Project/Helpers/DatabaseHelper.cs
+++ INFT3970-Project/INFT3970-Project/Helpers/DatabaseHelper.cs
@@ -626,5 +626,61 @@ namespace INFT3970Project.Helpers
             return false;
         }
 
+
+        public bool AddRoom(RoomModel model)
+        {
+            if (model.Name != null && model.Description != null)
+            {
+                var success = false;
+                try
+                {
+                    using (var _databaseHelper = new DatabaseHelper(configuration))
+                    {
+                        var command = new SqlCommand
+                        {
+                            Connection = (SqlConnection)_databaseHelper.Connection,
+                            CommandType = CommandType.StoredProcedure,
+                            CommandText = "dbo.AddRoom"
+                        };
+                        command.Parameters.AddWithValue("@Name", model.Name);
+                        command.Parameters.AddWithValue("@Description", model.Description);
+
+
+                        SqlParameter output = new SqlParameter("@responseMessage", SqlDbType.VarChar);
+                        output.Direction = ParameterDirection.Output;
+                        output.Size = 255;
+                        command.Parameters.Add(output);
+                        command.Connection.Open();
+                        command.ExecuteNonQuery();
+                        var response = command.Parameters["@responseMessage"].Value;
+                        var valid = (response.ToString() == "Invalid email") ? false :
+                            (response.ToString() == "Success") ? true : false;
+                        success = true;
+                        return valid;
+                    }
+                }
+                catch (Exception exception)
+                {
+                    using (var _databaseHelper = new DatabaseHelper(configuration))
+                    {
+                        _databaseHelper.Log("Fatal", exception.Message, null, GetCurrentMethod());
+                    }
+                    success = false;
+                    return false;
+                }
+                finally
+                {
+                    if (success)
+                    {
+                        using (var _databaseHelper = new DatabaseHelper(configuration))
+                        {
+                            _databaseHelper.Log("Infomation", $"Sensor Modified: {model.Name}", null, GetCurrentMethod());
+                        }
+                    }
+                }
+            }
+            return false;
+        }
+
     }
 }
