6eb37ea71f23ffc88840fc21d5c02e38900fe293
ShibbyMonster <c3283797@uon.edu.au>
2018-10-22 18:08:55 +1100

    DatabaseHelper fixed, Controller still not working

--- INFT3970-Project/INFT3970-Project/Models/Database Entities/UpdateUserDetailsModel.cs
+++ INFT3970-Project/INFT3970-Project/Models/Database Entities/UpdateUserDetailsModel.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using System.ComponentModel.DataAnnotations;
 
-namespace INFT3970Project.Models.Database_Entities
+namespace INFT3970Project.Models
 {
     public class UpdateUserDetailsModel
     {
