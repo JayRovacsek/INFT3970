d60a90462d6d564a8e5b7f43765448b5849cedbc
DeanMorty <c3252227@uon.edu.au>
2018-10-25 17:45:29 +1100

    Half done home page

---
+++ INFT3970-Project/INFT3970-Project/Models/Database Entities/CurrentTempModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace INFT3970Project.Models
+{
+    public class CurrentTempModel
+    {
+        public string Temp { get; set; }
+        public string Humidity { get; set; }
+        // Room Name
+        public string Expr1 { get; set; }
+        // Sensor Name
+        public string Name { get; set; }
+    }
+}
