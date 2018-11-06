6db785d36f9a641e5cfe820de51609bedd56ff2a
Jay Rovacsek <jayrovacsek@gmail.com>
2018-11-06 22:22:09 +1100

    Humidity proc implemented, optimisation of data... kinda.

---
+++ INFT3970-Project/INFT3970-Project/Models/Proceedure Models/AverageTemperatureModelWithId.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace INFT3970Project.Models
+{
+    public class AverageTemperatureModelWithId
+    {
+        public int SensorId { get; set; }
+        public double Temperature { get; set; }
+        public DateTime StartTime { get; set; }
+        public DateTime EndTime { get; set; }
+    }
+}
---
+++ INFT3970-Project/INFT3970-Project/Models/Proceedure Models/MotionCountModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace INFT3970Project.Models
+{
+    public class MotionCountModel
+    {
+        public int MotionCount { get; set; }
+        public DateTime StartTime { get; set; }
+        public DateTime EndTime { get; set; }
+    }
+}
