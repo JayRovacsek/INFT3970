08399191bdf4a4bc8a46a9c4c103f5336cb68e4f
Jay Rovacsek <jayrovacsek@gmail.com>
2018-11-08 18:44:40 +1100

    Predictive humidity added

--- INFT3970-Project/INFT3970-Project/Helpers/DatabaseHelper.cs
+++ INFT3970-Project/INFT3970-Project/Helpers/DatabaseHelper.cs
@@ -1375,30 +1375,29 @@ namespace INFT3970Project.Helpers
                                         DECLARE @SearchStartTime datetime;
                                         SET @SearchStartTime = DATEADD(week,-1,@SearchEndTime)
 
-		                                SELECT StartTime, LAG(PredictedValue, 1, PredictedValue) OVER (ORDER BY StartTime) + (LAG(PredictedValue, 1, 0) OVER (ORDER BY StartTime) * (PercentChange)) AS 'Temperature'
-		                                FROM (
-		                                    SELECT HourlyAverage, StartTime,  PercentChange, ID, HourlyAverage as PredictedValue
-		                                    FROM (
-						                        SELECT HourlyAverage, StartTime,- 1 * (1 - Lag(HourlyAverage, 1, 0) OVER (Order by StartTime) / HourlyAverage) AS PercentChange, ID, EndTime
-						                        FROM (
-								                    SELECT AVG(Temp) AS HourlyAverage, StartTime, ROW_NUMBER() OVER (ORDER BY StartTime) AS ID, EndTime
-								                    FROM (
-										                SELECT TempID, StartTime, Temp, StartTime + '00:59:59' AS EndTime
-										                FROM (
-										                    SELECT TempID, DATEADD(hh,DATEDIFF(hh,0,t.[Date]),0) AS StartTime, Temp, s.SensorID
-										                    FROM Temperature t
-										                    INNER JOIN Sensor s ON  s.SensorID = t.SensorID
-										                    WHERE t.SensorID = {sensor.SensorId} and  t.[Date] BETWEEN @SearchStartTime AND @SearchEndTime 
-										                    GROUP BY t.TempID, t.[Date], t.Temp, s.SensorID ) Temperature
-										                INNER JOIN Sensor s ON s.SensorID = Temperature.SensorId 
-									                    GROUP BY TempID, StartTime, Temp ) Temperature
-								                        WHERE StartTime BETWEEN StartTime AND EndTime
-								                    GROUP BY StartTime, EndTime ) Temperature
-							                        WHERE StartTime BETWEEN StartTime AND EndTime
-							                    GROUP BY HourlyAverage, StartTime, ID, EndTime )			
-					                        Temperature
-					                        GROUP BY HourlyAverage, StartTime, PercentChange, ID ) Temperature
-                                            ORDER BY StartTime";
+                                        SELECT StartTime, LAG(PredictedValue, 1, PredictedValue) OVER (ORDER BY StartTime) + (LAG(PredictedValue, 1, 0) OVER (ORDER BY StartTime) * (PercentChange)) as 'Humidity'
+                                        FROM (
+	                                        SELECT HourlyAverage, StartTime,  PercentChange, ID, HourlyAverage as PredictedValue
+	                                        FROM (
+		                                        SELECT HourlyAverage, StartTime,- 1 * (1 - Lag(HourlyAverage, 1, 0) OVER (ORDER BY StartTime) / HourlyAverage) AS PercentChange, ID, EndTime
+		                                        FROM (
+			                                        SELECT AVG(Humidity) AS HourlyAverage, StartTime, ROW_NUMBER() OVER (ORDER BY StartTime) AS ID, EndTime
+			                                        FROM (
+				                                        SELECT HumidityID, StartTime, Humidity, StartTime + '00:59:59' AS EndTime
+				                                        FROM (
+					                                        SELECT HumidityID, DATEADD(hh,DATEDIFF(hh,0,h.[Date]),0) AS StartTime, Humidity, s.SensorID
+					                                        FROM Humidity h
+					                                        INNER JOIN Sensor s ON  s.SensorID = h.SensorID
+					                                        WHERE h.SensorID = {sensor.SensorId} and  h.[Date] BETWEEN @SearchStartTime AND @SearchEndTime 
+		                                        GROUP BY h.HumidityID, h.[Date], h.Humidity, s.SensorID ) Humidity
+		                                        INNER JOIN Sensor s ON s.SensorID = Humidity.SensorId 
+		                                        GROUP BY HumidityID, StartTime, Humidity ) Humidity
+	                                        WHERE StartTime BETWEEN StartTime AND EndTime
+	                                        GROUP BY StartTime, EndTime) Humidity
+                                        WHERE StartTime BETWEEN StartTime AND EndTime
+                                        GROUP BY HourlyAverage, StartTime, ID, EndTime ) Humidity
+                                        GROUP BY HourlyAverage, StartTime, PercentChange, ID ) Humidity 
+                                        ORDER BY StartTime";
 
                         var dbResult = _databaseHelper.Connection.Query<AverageHumidityModel>(command);
 
